8cd4882901bb683a1f1579631b0b12b6b6046243
Alba Alcaniz <albaalcanizmoya@gmail.com>
2024-03-04 15:06:01 +0100

    Start documenting the class Sea

--- pvlib/floating/floating.py
+++ pvlib/floating/floating.py
@@ -1,137 +1,168 @@
-import numpy as np
+"""
+The ``floating`` module contains methods to calculate the varying inclinations
+of a floater on sea
+Methodology developed in A. Alcañiz et al. "Offshore floating PV - DC and AC 
+yield analysis considering wave effects" in Energy Conversion and Management
 
-# Default values for Jonswap spectrum
-# Literature data from [1]
-WindSpeed = np.arange(0, 91, 10) / 3.6  # m/s
-WaveHeight = [0, 0.25, 0.5, 1.2, 2.5, 4.5, 7.1, 10.3, 14.3, 19.3]  # m
-AveragePeriod = [0, 1.6, 3.2, 4.6, 6.2, 7.7, 9.9, 10.8, 12.4, 13.9]  # s
-# Extract the parameters of the interpolation
-c_wh = np.polyfit(WindSpeed, WaveHeight, 3)
-c_avp = np.polyfit(WindSpeed, AveragePeriod, 1)
-# Peakedness parameter
-Gamma = 3.3
-Beta = 5 / 4
-# Spectral width parameter
-SigmaA = 0.07
-SigmaB = 0.09
+TODO: ensure that it works also with pandas inputs
+TODO: include the pierson-moskowitz spectrum
+"""
+
+# Alba Alcañiz, Delft University of Technology 2024
+
+import numpy as np
+from pvlib.tools import sind, cosd, tand
 
 # Gravitational acceleration [m/s^2]
-g = 9.806
+# g = 9.806
 
 def decompose_wind_speed(wind_speed, wind_dir, facing_dir):
-    """_summary_
+    """
+    Decompose the wind speed into x and y components following a determined 
+    direction
 
-    Args:
-        wind_speed (_type_): _description_
-        wind_dir (_type_): _description_
-        float_orient (_type_): _description_
+    Parameters
+    ----------
+    wind_speed : numeric
+        Wind speed [m/s]
+    wind_dir : numeric
+        Wind direction [degrees]
+    facing_dir : numeric
+        Facing direction of the floater over which the wind is decomposed. Same 
+        convention for the angles as the wind direction
 
-    Returns:
-        _type_: _description_
+    Returns
+    -------
+    wind_speed_x : numeric
+        Wind speed parallel to the facing direction [m/s]
+    wind_speed_y : numeric
+        Wind speed perpendicular to the facing direction [m/s]
     """
     # Decompose the wind speed into components
-    wind_speed_x = abs(wind_speed * np.cos(np.deg2rad(wind_dir - facing_dir)))
-    wind_speed_y = abs(wind_speed * np.sin(np.deg2rad(wind_dir - facing_dir)))    
+    wind_speed_x = abs(wind_speed * cosd(wind_dir - facing_dir))
+    wind_speed_y = abs(wind_speed * sind(wind_dir - facing_dir))
     return wind_speed_x, wind_speed_y
 
 
-class Sea:    
+class Sea:
+    """
+    Sea objects are containers for spectra and surface elevation associated to 
+    a particular water body.
+
+    Parameters
+    ----------
+    dw : float
+        Step in the angular frequency, default 0.01 [rad/s]
+    w_max: float
+        Maximum angular frequency, default 8 [rad/s]
+    dt : float
+        Time step that provides the time resolution of the final angle outputs,
+        default 1 [s]
+    t_dur : float
+        Number of dt that are in each wind_speed value, e.g. if the wind data 
+        has hourly resolution and dt = 1 s, t_dur should be 3600, default 3600
+    density : float
+        Sea water density, default 1029 [kg/m3]
+    """
     def __init__(self, dw=0.01, w_max=8, dt=1, t_dur=3600, density=1029):
-        # Angular frequency [rad/s]
+        """
+        ang_freq : numeric
+            Range of angular frequencies over which the spectrum is computed
+        time : numeric
+            Time period indicating the correspondence in resolution between the 
+            input wind data and the output results
+        wave_num : numeric
+            Wave number associated to the angular frequency [1/m]
+        """
         self.ang_freq = np.arange(dw, w_max + dw, dw)
-        # Time period [s]
         self.time = np.arange(dt, t_dur + dt, dt)
-        # Dispersion relation to obtain the wave number [1/m]
-        self.wave_num = self.ang_freq**2 / g
-        # Sea water density
+        self.wave_num = self.ang_freq**2 / 9.806
         self.density = density
           
     def jonswap_spectrum(self, wind_speed):
-        """JONSWAP_SPECT Create the JONSWAP spectra
-        % Description:
-        %   Create the JONSWAP spectra from the time-resolved wind speed and
-        %   angular frequency. Since the fetch and period are hard to measure, the
-        %   equations of the spectra have been modified to use only the significant
-        %   wave height and the average period, which can be extracted from the
-        %   wind speed via interpolation of the literature data
-        %
-        % Note:
-        %   Function adapted from the one created by Sayyed Mohsen Vazirizade from
-        %   the University of Arizona
-        %   smvazirizade@email.arizona.edu
-        %
-        % Inputs:
-        %   wind_speed - time series of wind speed in m/s
-        %
-        % Outputs:
-        %   S - spectra for every time series and angular frequency in m^2*s
-        %   peak_ang_freq - peak angular frequency in rad/s
+        '''
+        Obtain the JONSWAP spectra [1] from the time-resolved wind speed and 
+        angular frequency. Since the fetch and period are hard to measure, the
+        equations of the spectra have been modified to use only the significant
+        wave height and the average period, which can be extracted from the
+        wind speed via interpolation of literature data.
+        
+        Parameters
+        ----------
+        wind_speed : numeric
+            Wind speed [m/s]
 
-            Args:
-                ang_freq (_type_): _description_
-                wind_speed (_type_): _description_
-                g (_type_): _description_
+        Returns
+        -------
+        spectrum : numeric
+            Spectra for every time step and angular frequency [m2*s]
+        peak_ang_freq : numeric
+            Peak angular frequency [rad/s]
 
-            Returns:
-                _type_: _description_
-        """
-        # Compute the significant wave height [m] and the average period [s] for
-        # each component as a function of wind speed
-        Hs, Tz = self.jonswap_params(wind_speed)
+        Notes
+        -----
+        Function adapted from the one created by Sayyed Mohsen Vazirizade from
+        the University of Arizona
+        smvazirizade@email.arizona.edu
+        
+        References
+        ----------
+        .. [1] Yu, Y., Pei, H. & Xu, C. "Parameter identification of JONSWAP 
+           spectrum acquired by airborne LIDAR" J. Ocean Univ. China 16, 
+           998–1002 (2017) https://doi.org/10.1007/s11802-017-3271-2
+        '''
+        sign_wave_height, avg_period = self.jonswap_params(wind_speed)
 
-        # Convert wave period Tz to peak wave period Tm
-        Tm = Tz * (0.327 * np.exp(-0.315 * Gamma) + 1.17)
-
-        # Compute peak angular frequency
-        peak_ang_freq = 2 * np.pi / Tm
+        peak_period = avg_period * 1.2856
+        peak_ang_freq = 2 * np.pi / peak_period
         peak_ang_freq = peak_ang_freq.reshape(len(peak_ang_freq),1)
 
-        # Jonswap spectrum
         ang_freq = self.ang_freq.reshape(1,len(self.ang_freq))
-        sigma = (ang_freq <= peak_ang_freq) * SigmaA + (ang_freq > peak_ang_freq) * SigmaB
-        A = np.exp(-(((ang_freq / peak_ang_freq - 1) / (sigma * np.sqrt(2))) ** 2))
-        # Modified Phillips constant
-        alphabar = 5.058 * (1 - 0.287 * np.log(Gamma)) * (Hs / Tm**2) ** 2
-        alphabar = alphabar.reshape(len(alphabar),1)
-        # Spectra m^2.s
-        spectrum = alphabar * g**2 * ang_freq**-5 * np.exp(-(Beta * (ang_freq / peak_ang_freq) ** -4)) * Gamma**A
+        sigma = (0.07 * (ang_freq <= peak_ang_freq) + 
+                 0.09 * (ang_freq > peak_ang_freq))
+        peak_enh_fact = 3.3**np.exp(-((ang_freq / peak_ang_freq - 1) / 
+                                      (sigma * np.sqrt(2))) ** 2)
+        mod_phillips_ct = 3.32484722 * (sign_wave_height / peak_period**2) ** 2
+        mod_phillips_ct = mod_phillips_ct.reshape(len(mod_phillips_ct),1)
+        spectrum = (mod_phillips_ct * 96.15764 * ang_freq ** -5 *
+                    np.exp(-(1.25 * (ang_freq / peak_ang_freq) ** -4)) * 
+                    peak_enh_fact)
 
         return spectrum, peak_ang_freq
     
 
     def jonswap_params(self,wind_speed):
-        """JONSWAP_PARAMS Obtain the needed JONSWAP parameters from the wind speed
-        %
-        % Syntax:
-        %   [H,T] = jonswap_params(wind_speed)
-        %
-        % Description:
-        %   Obtain the significant wave height and the average period, needed to
-        %   calculate the JONSWAP spectra, from the wind speed via interpolation of
-        %   data from the literature [1]
-        %
-        % Citations:
-        %   [1] P. R. Pinet "Essential invitation to oceanography" Jones & Bartlett
-        %     Publishers, 2014
-        %
-        % Inputs:
-        %   wind_speed - time-resolved wind speed data in m/S
-        %
-        % Outputs:
-        %   H - time-resolved significant wave height [m]
-        %   T - time-resolved average period [s]
+        '''
+        Obtain the significant wave height and the average period, needed to
+        calculate the JONSWAP spectra, from the wind speed via interpolation of
+        data from the literature [1].
 
-            Args:
-                wind_speed (_type_): _description_
+        Parameters
+        ----------
+        wind_speed : numeric
+            Wind speed [m/s]
 
-            Returns:
-                _type_: _description_
-        """
+        Returns
+        -------
+        sign_wave_height : numeric
+            Significant wave height [m]
+        avg_period : numeric
+            Average wave period [s]
+    
+        References
+        ----------
+        .. [1] P. R. Pinet "Essential invitation to oceanography" Jones & 
+           Bartlett Publishers, 2014
+        '''
+        WIND_SPEED_LIT = np.arange(0, 91, 10) / 3.6
+        WAVE_HEIGHT_LIT = [0, 0.25, 0.5, 1.2, 2.5, 4.5, 7.1, 10.3, 14.3, 19.3]
+        AVG_PERIOD_LIT = [0, 1.6, 3.2, 4.6, 6.2, 7.7, 9.9, 10.8, 12.4, 13.9]
+        c_wh = np.polyfit(WIND_SPEED_LIT, WAVE_HEIGHT_LIT, 3)
+        c_avp = np.polyfit(WIND_SPEED_LIT, AVG_PERIOD_LIT, 1)
 
-        # Perform the interpolation with the input wind speed data
-        H = np.polyval(c_wh, wind_speed)
-        T = np.polyval(c_avp, wind_speed)
-        return H, T
+        sign_wave_height = np.polyval(c_wh, wind_speed)
+        avg_period = np.polyval(c_avp, wind_speed)
+        return sign_wave_height, avg_period
 
     
     def pierson_moskowitz_spectrum(self, wind_speed):
@@ -139,28 +170,101 @@ class Sea:
         return spectrum
     
 
-    def surface_elevation(self, type_spectrum, wind_speed, x):
+    def surface_elevation(self, wind_speed, x, type_spectrum='JONSWAP'):
+        '''
+        Generate a statistically possible sea surface elevation from the sea
+        spectrum.
+
+        Parameter ``type_spectrum`` allows selection of different sea spectra.
+
+        Parameters
+        ----------
+        wind_speed : numeric
+            Wind speed [m/s]
+        x : float
+            Location at which the wave is evaluated [m]
+        type_spectrum : string, default 'JONSWAP'
+            Available spectra include the following:
+
+            * 'JONSWAP' - Spectrum for the North Sea [1]
+            * 'Pierson-Moskowitz' - TODO: Under development
+
+        Returns
+        -------
+        elevation : numeric
+            Time-resolved statistically possible sea surface elevation [m]
+        AmpA : numeric
+            Amplitude of the surface elevation related to the cosine [m]
+        AmpB : numeric
+            Amplitude of the surface elevation related to the sine [m]
+
+        Notes
+        -----
+        Function adapted from the one created by Sayyed Mohsen Vazirizade from
+        the University of Arizona
+        smvazirizade@email.arizona.edu
+        
+        References
+        ----------
+        .. [1] Yu, Y., Pei, H. & Xu, C. "Parameter identification of JONSWAP 
+           spectrum acquired by airborne LIDAR" J. Ocean Univ. China 16, 
+           998–1002 (2017) https://doi.org/10.1007/s11802-017-3271-2
+        '''
         if type_spectrum == 'JONSWAP':
             spectrum = self.jonswap_spectrum(wind_speed)[0]
-        elif type_spectrum == 'Pierson-Moskowitz':
-            spectrum = self.pierson_moskowitz_spectrum(wind_speed)
+        # elif type_spectrum == 'Pierson-Moskowitz':
+        #     spectrum = self.pierson_moskowitz_spectrum(wind_speed)
+        else:
+            raise ValueError('%s is not a valid model for sea spectrum', 
+                             type_spectrum)
         
         n_w = len(self.ang_freq)
         dw = self.ang_freq[1] - self.ang_freq[0]
         X = x * np.ones(self.time.shape)
-        # AmpA = (spectrum*dw)**0.5 * np.random.normal(size=(1,n_w))
-        # AmpB = (spectrum*dw)**0.5 * np.random.normal(size=(1,n_w))
-        AmpA = (spectrum*dw)**0.5 * np.ones((1,n_w))
-        AmpB = (spectrum*dw)**0.5 * np.ones((1,n_w))
-        arg_rad = (self.wave_num.reshape(len(self.wave_num),1))@(X.reshape(1,len(X))) - (self.ang_freq.reshape(len(self.ang_freq),1))@(self.time.reshape(1,len(self.time)))
-        elevation = AmpA@np.cos(arg_rad) + AmpB@np.sin(arg_rad)
+        # AmpA = (spectrum * dw) ** 0.5 * np.random.normal(size=(1, n_w))
+        # AmpB = (spectrum * dw) ** 0.5 * np.random.normal(size=(1, n_w))
+        AmpA = (spectrum * dw) ** 0.5 * np.ones((1, n_w))
+        AmpB = (spectrum * dw) ** 0.5 * np.ones((1, n_w))
+        arg_rad = ((self.wave_num.reshape(len(self.wave_num),1)) @
+                   (X.reshape(1, len(X))) - 
+                   (self.ang_freq.reshape(len(self.ang_freq),1)) @
+                   (self.time.reshape(1, len(self.time))))
+        elevation = AmpA @ np.cos(arg_rad) + AmpB @ np.sin(arg_rad)
 
         return elevation, AmpA, AmpB
     
-    def get_sea_amplitudes(self, type_spectrum, wind_speed_x, wind_speed_y, x):
-        AmpA_x, AmpB_x = self.surface_elevation(type_spectrum, wind_speed_x, x)[1:3]
-        AmpA_y, AmpB_y = self.surface_elevation(type_spectrum, wind_speed_y, x)[1:3]
-        return [AmpA_x, AmpB_x, AmpA_y, AmpB_y]
+
+    def get_sea_amplitudes(self, wind_speed_x, wind_speed_y, x, 
+                           type_spectrum='JONSWAP'):
+        '''
+        Get the sea amplitudes at a specified location given the wind speed.
+
+        Parameters
+        ----------
+        wind_speed_x : numeric
+            Wind speed in the x-direction [m/s]
+        wind_speed_y : numeric
+            Wind speed in the perpendicular direction of ``wind_speed_x`` [m/s]
+        x : float
+            Location at which the wave is evaluated [m]
+        type_spectrum : string, default 'JONSWAP'
+            Available spectra include the following:
+
+            * 'JONSWAP' - Spectrum for the North Sea [1]
+            * 'Pierson-Moskowitz' - TODO: Under development
+
+        Returns
+        -------
+        amplitudes : list
+            Amplitudes in the x and y directions and for the sine and cosine 
+            components of the sea elevation
+        '''
+        AmpA_x, AmpB_x = self.surface_elevation(type_spectrum, wind_speed_x, 
+                                                x)[1:3]
+        AmpA_y, AmpB_y = self.surface_elevation(type_spectrum, wind_speed_y, 
+                                                x)[1:3]
+        amplitudes = [AmpA_x, AmpB_x, AmpA_y, AmpB_y]
+        return amplitudes
     
 
 class Floater:
@@ -187,7 +291,7 @@ class Floater:
             wid = self.width; leng = self.length
         elif axis == 'y':
             wid = self.length; leng = self.width
-        fact = 2 * Sea.density * g * wid / I
+        fact = 19.612 * Sea.density * wid / I
         xx = Sea.wave_num * leng / 2
         aux1 = 1 / (Sea.wave_num**2 * Sea.ang_freq**2) * (np.sin(xx) - xx * np.cos(xx))
         aux1 = aux1.reshape(len(aux1),1)
